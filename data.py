b09afeb9ad6b9030034ad5ffb5fa6c908a739fc8
Taylor Torres <taylor23torres@gmail.com>
2021-01-16 19:07:50 -0600

    adds pages

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem',
+            'author': 'Taylor Torres',
+            'create_date': '01-16-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem',
+            'author': 'Hannah Ward',
+            'create_date': '01-16-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem',
+            'author': 'Hannah Portokalos',
+            'create_date': '01-16-2021'
+        }
+    ]
+    return articles
